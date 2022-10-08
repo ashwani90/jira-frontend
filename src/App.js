9e7fbabaf4710364c33ee975f57278057421bd04
Ashwani <ashwani90125@gmail.com>
2022-10-08 21:06:07 +0530

    Work on add settings page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
 import React from 'react';
 import './App.css';
 import Home from "./containers/pages/Home";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 function App() {
   return (
---
+++ src/containers/pages/Settings.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Settings() {
+    return (
+        <>
+        Hello
+        </>
+    );
+}
+
+export default Settings;
